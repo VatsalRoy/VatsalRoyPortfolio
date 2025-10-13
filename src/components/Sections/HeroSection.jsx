53ec59cf984106b0d9d6456f5778a626da2b5770
VatsalRoy <vatsalroy293@gmail.com>
2025-10-13 15:06:06 -0400

    Email update on hero section

--- src/components/Sections/HeroSection.jsx
+++ src/components/Sections/HeroSection.jsx
@@ -130,7 +130,7 @@ const HeroSection = () => {
                                 {[
                                     {icon: FiGithub, href: 'https://github.com/VatsalRoy'},
                                     {icon: FiLinkedin, href: 'https://www.linkedin.com/in/vatsal-roy/'},
-                                    {icon: Mail, href: 'mailto:vatsalroy2429@gmail.com'},
+                                    {icon: Mail, href: 'mailto:vatsal.roy2429@gmail.com'},
                                     {icon: FileUser, href: 'https://drive.google.com/file/d/1cx222SwiVRMXvjkpCXrM0yftJJ-0UcYZ/view?usp=sharing'},
                                 ].map((social, index) => (
                                     <motion.a
